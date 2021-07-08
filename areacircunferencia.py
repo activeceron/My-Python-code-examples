c3c7786b533229fcb0092dd0c02735f3ff646e51
César LR Padilla <86532763+activeceron@users.noreply.github.com>
2021-07-08 15:15:41 +0200

    Calcula el área de una circunferencia

---
+++ areacircunferencia.py
@@ -0,0 +1,9 @@
+#Programa que calcula el área de un círculo.
+# Python Practice (@laborion) -- https://t.me/python_practice
+# https://github.com/activeceron
+import math
+radio=float(input("Introduzca radio circunferencia:"))
+area=2 * math.pi * radio
+print(f" El area es {area:.2f}")
+print("Pulsa enter para salir")
+input()
